7d7fa1c89e384a7e0565a0cc13c5e6f67f1c96b4
tintinrevient <zhaobenben007@googlemail.com>
2024-04-25 12:58:29 +0200

    debug with streamlit

---
+++ chat.py
@@ -0,0 +1,20 @@
+from langchain_community.callbacks import StreamlitCallbackHandler
+import streamlit as st
+import requests
+
+
+def chat_ui():
+
+    if question := st.chat_input():
+        st.chat_message("user").write(question)
+
+        with st.chat_message("bear"):
+            st_callback = StreamlitCallbackHandler(st.container())
+
+            response = requests.post("http://localhost:8000/chat", json={"text": question, "user_id": "Emmy"}).json()
+            st.write(response["output_message"])
+
+
+if __name__ == "__main__":
+    chat_ui()
+

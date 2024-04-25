fb37e1635bc853335946159ede1e2ea0cf102ce3
JSchuurmans <jetzeschuurmans@gmail.com>
2024-04-25 16:51:41 +0200

    audio to chain to audio:

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ async def root(prompt: Prompt):
         "user_id": prompt.user_id,
     }
 
-@app.post("/audio")
-async def audio(file: UploadFile = File(...)):
+@app.post("/audio_text")
+async def audio_text(file: UploadFile = File(...), user_id: str = "Emmy"):
     client = SpeechClient()
     model="latest_long"
     project_id="qwiklabs-gcp-02-c86bd22dbd03"
@@ -59,17 +59,105 @@ async def audio(file: UploadFile = File(...)):
 
     for result in response.results:
         print(f"Transcript: {result.alternatives[0].transcript}")
+        text = result.alternatives[0].transcript
+        print(f"{text = }")
+        break
 
+    context = get_full_patient_context(user_id)
+    # print(context)
+    output = chatbot.invoke(
+        {"input": text, "context": context},
+        config={"configurable": {"user_id": user_id}},
+    )
     # TODO: pass transcript to langchain
-    chain_output = root(result.alternatives[0].transcript)
-    print(f"chain_output: {chain_output}")
+    # chain_output = root(result.alternatives[0].transcript)
+    # print(f"chain_output: {chain_output}")
 
     # TODO: pass output of langchain 
     # chain_output["output_message"]
 
     return {
-        "text": result.alternatives[0].transcript
+        # "input_message": prompt.text,
+        "output_message": transform_numbers_to_text(output.content), # TODO transform "8" into eight
+        "user_id": user_id,
     }
+
+
+@app.post("/audio")
+async def audio(file: UploadFile = File(...), user_id: str = "Emmy"):
+    client = SpeechClient()
+    model="latest_long"
+    project_id="qwiklabs-gcp-02-c86bd22dbd03"
+
+    content = await file.read()
+
+    config = cloud_speech.RecognitionConfig(
+        auto_decoding_config=cloud_speech.AutoDetectDecodingConfig(),
+        language_codes=["en-US"],
+        model=model,
+    )
+
+    request = cloud_speech.RecognizeRequest(
+        recognizer=f"projects/{project_id}/locations/global/recognizers/_",
+        config=config,
+        content=content,
+    )
+
+    # Transcribes the audio into text
+    response = client.recognize(request=request)
+
+    for result in response.results:
+        print(f"Transcript: {result.alternatives[0].transcript}")
+        text = result.alternatives[0].transcript
+        print(f"{text = }")
+        break
+
+    context = get_full_patient_context(user_id)
+    # print(context)
+    output = chatbot.invoke(
+        {"input": text, "context": context},
+        config={"configurable": {"user_id": user_id}},
+    )
+    # TODO: pass transcript to langchain
+    # chain_output = root(result.alternatives[0].transcript)
+    # print(f"chain_output: {chain_output}")
+
+    # TODO: pass output of langchain 
+    # chain_output["output_message"]
+
+    ##################
+    # text to speech #
+    ##################
+    text = output.content
+
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=text)
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    # Set the content type of the response to audio/mpeg
+    headers = {
+        "Content-Disposition": "attachment; filename=output.mp3",
+        "Content-Type": "audio/mpeg",
+    }
+
+    return Response(content=response.audio_content, media_type="audio/mpeg", headers=headers)
 
 
 @app.post("/text_audio")
@@ -119,7 +207,6 @@ async def text_audio(prompt: Prompt):
         "Content-Disposition": "attachment; filename=output.mp3",
         "Content-Type": "audio/mpeg",
     }
-
 
     return Response(content=response.audio_content, media_type="audio/mpeg", headers=headers)
 

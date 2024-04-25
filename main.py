004082c60590e005b2db48ac08400dde02f3bad1
JSchuurmans <jetzeschuurmans@gmail.com>
2024-04-25 10:57:25 +0200

    patient context

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from maximai.langchain import create_context_aware_chatbot, get_context
+from maximai.langchain import create_context_aware_chatbot, get_patient_context
 from maximai.schemas import Prompt
 
 app = FastAPI(title="MaximAI Chat App")
@@ -12,7 +12,7 @@ chatbot = create_context_aware_chatbot()
 @app.post("/chat")
 async def root(prompt: Prompt):
     output = chatbot.invoke(
-        {"input": prompt.text, "context": get_context(prompt.user_id)},
+        {"input": prompt.text, "context": get_patient_context(prompt.user_id)},
         config={"configurable": {"user_id": prompt.user_id}},
     )
     output = output["output"]
--- src/maximai/langchain.py
+++ src/maximai/langchain.py
@@ -34,6 +34,22 @@ def get_context(user_id: str) -> str:
     return info[user_id]
 
 
+def get_patient_context(user_id: str) -> str:
+    info = {
+        "Emmy": "Emmy, 8 years old, diagnosed with osteosarcoma in left shoulder, treatment according to EURAMOS 1 protocol, successful resection of tumor, received treatment with doxorubicin, cisplatin, methotrexate, ifosfamide, and etoposide is now in the 25th week of treatment. She experiences loss of functionality in left arm, has pain and is anxious.",
+        "David": "David, 3 years old, diagnosed with neuroblastoma stadium IV, or INRG stadium HR, treatment with DCOG NBL, in his 5th N5 cycle. He receives tube feeding and is very nauseous. He vomits 4-5 time a day and the tube feeding is frequently interrupted.",
+        "Jenny": "Jenny, 6 years old, diagnosed with Wilms tumor stadium IV, resection of kidney, receives vincristine, doxorubicin, actinomycin. She is normally a very active child, but currently she suffers from abdominal pain and stays in bed whole day.",
+        "Jan": "Jan, a 12 years old boy with medulloblastoma, brain surgery and treatment with COG ACNS 0331 and radiation. Experiences headache, fatigue and nausea.",
+        "Bjorn": "Bjorn, 16 years old, diagnosed with craniopharyngioma, a very wise and talkative person. Brain surgery and partly resection of the tumor. It is one week after surgery, he experiences drowsiness and nausea when he leaves his bed. Despite his restrictions, needs help to go to the bathroom and experiences difficulty walking, he is happy and grateful that he survived surgery.",
+        "Roy": "Roy, 15 years, diagnosed with Hodgkin lymphoma (cHL), treatment according to DECOPDAC-21. He received two cycles COPP-ABV: (cyclophosphamide, vincristine, procarbazine, prednisone, doxorubicin, bleomycin, vinblastine). Roy is a quiet and shy person, he is not very talkative. He has no appetite, experiences difficulty with sleeping and feels very tired.",
+        "Daisy": "Daisy, 17 years, diagnosed with non-Hodgkin lymphoma (DLBCL). Treatment with cyclophosphamide, MTX and ARA-C. She used to have long blond hair and she feels miserable about being bald and about her changed appearance. She often feels sick and tired.",
+        "Ella": "Ella, 3 years, diagnosed with AML, treatment protocol NOPHO-DBH AML 2012. She has finished FLA cycle (fludarabine, cytarabine and MTX intrathecal). She does not want to eat or talk, is anxious and sits on her mother’s lap when the nurse enters the room.",
+        "Peter": "Peter, 10 years old, diagnosed with ALL SR and treated according to ALLtogether 1, end of induction (received dexamethasone). He is passive and inactive and prefers fries for diner. He has sometimes constipation and abdominal pain.",
+        "Thom": "Thom, 4 years old, diagnosed with DIPG 7 months ago. He is in incurable and in palliative phase of his disease. He has difficulty walking, and talking. He is tired and needs assistance for quiet play. He is drowsy and feels often nauseous and has a headache.",
+    }
+    return info[user_id]
+
+
 def get_user_history(user_id: str) -> BaseChatMessageHistory:
     if user_id not in store:
         store[user_id] = ChatMessageHistory()
@@ -51,7 +67,7 @@ def create_chat_chain() -> Runnable:
         [
             (
                 "system",
-                "The user's favourite food is {context}",
+                "{context}",
             ),
             MessagesPlaceholder(variable_name="history"),
             ("human", "{input}"),
